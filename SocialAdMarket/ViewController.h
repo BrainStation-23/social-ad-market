bbe46347cf7f2aaddd03c5e60a9e7d1fb9f3963c
Mahjabin Alam <mahjabin@brainstation-23.com>
2015-10-19 16:23:07 +0600

    Initial Commit

---
+++ SocialAdMarket/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialAdMarket
+//
+//  Created by Mahjabin Alam on 10/19/15.
+//  Copyright (c) 2015 Brainstation-23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocialAdMarket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocialAdMarket
+//
+//  Created by Mahjabin Alam on 10/19/15.
+//  Copyright (c) 2015 Brainstation-23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
